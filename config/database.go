b267a85f91dd6911d0be0eaf0e143d012c81b1a6
Tanya Dixit <tanya.d.dixit@oracle.com>
2025-02-24 00:54:08 +0530

    Created database connection

--- config/database.go
+++ config/database.go
@@ -1,1 +1,31 @@
 package config
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func DatabaseConnection() *gorm.DB {
+	dsn := "myuser:password@tcp(localhost:3306)/library?charset=utf8mb4&parseTime=True&loc=Local"
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to MySQL:", err)
+	}
+	fmt.Println("Connected to MySQL successfully!")
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatal("Failed to get database instance:", err)
+	}
+	defer sqlDB.Close()
+	err = sqlDB.Ping()
+	if err != nil {
+		log.Fatal("Database is not reachable:", err)
+	}
+	fmt.Println("MySQL connection is alive!")
+	return db
+}
